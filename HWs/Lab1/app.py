562d5165d173b8fc00036fa24c0b295d53945f7c
Zef-Hub <gerasenkov1@mail.ru>
2024-03-05 20:59:07 +0300

    Diff

--- HWs/Lab1/app.py
+++ HWs/Lab1/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+application = app
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
