25cbec125937ccf3394d7fc1cadadb9eb68fa77f
Zef-Hub <gerasenkov1@mail.ru>
2024-03-06 20:15:08 +0300

    Add Lab1

--- HWs/Lab1/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-application = app
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
---
+++ HWs/Lab1/oldApp.py
@@ -0,0 +1,68 @@
+from flask import Flask
+
+app = Flask(__name__)
+application = app
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+{
+  "title": "Заголовок поста",
+  "text": "Personal operation point prepare could plan her worry. Another whole beautiful. Late significant position test office. Sport Mr rather. Outside floor strategy laugh nation me but item. Reflect win during recent common. Little try pretty bit federal wish project wrong. Three dog get. Site power only south recent sea. Town board own least player decision still. College community stock pass eight tree however. Ready hundred how crime recognize. Home budget girl guess. Myself newspaper at mind shake view land. Concern might tend ability allow. Decide environment cell agency report him. Improve like wind year main poor. That development usually close. President ball different remain appear join. Very guy word so two serious west. Yard once arm popular direction to. Also list view mean week across tough. Image buy specific move notice say. Area organization industry small bar service ten go. After unit book probably face music cell music. Artist study improve second oil Democrat word. Have must enough when. Card sense high city none indicate. Far win significant heart happen. Peace group information. Few protect shoulder identify guess majority energy child. Understand property anyone serve minute position. Else amount matter stay physical. Defense international minute type let per. Fine near community line enough point. Or street start senior discuss. Stay history win management. Good find share goal. News nature rise also fly just threat. Determine large bring serious. Him debate section child property. Onto discuss involve. Benefit whole left front important. Draw life attack than movement draw. Number enjoy although listen. Fear spend design strategy central. Adult candidate scientist. Front of cold strategy. Rather value control chair reveal federal. Hotel thank hard itself hot law. Hot arm eight reflect pattern allow point. Like interest staff sit follow everyone may whole. Field happy toward because develop traditional idea. I people step movie simple factor member. Center listen couple career yourself size. Tonight fall budget system room million newspaper. Treat any majority model past avoid question. Walk color responsibility parent reflect free side. Memory have smile bed behind move. Food daughter drug leg yet story box. Its eye parent. Talk man peace identify outside growth imagine break. Enjoy late arrive never nature recent vote. Investment example turn wonder maybe. Everybody fact step drive artist. Natural system as cell public see trouble cover. Dog arrive unit deep. Pm either brother participant. Both daughter morning class current camera. Join ok energy the maintain. My possible positive need. Law light tonight firm six one activity couple. Mean figure all decision employee measure. Medical scientist campaign term. President report by. Film try wear structure author. Professor stay include find. Democratic between south college per chair where. Trial already shoulder consumer compare rich. Community here act during enough. Car answer try guess exist office goal activity. Require example light animal party. Possible never recently. Under popular from American attorney.",
+  "author": "Jessica Knight",
+  "date": "datetime.datetime(2023, 6, 27, 5, 31, 24)",
+  "image_id": "7d4e9175-95ea-4c5f-8be5-92a6b708bb3c.jpg",
+  "comments": [
+    {
+      "author": "Jennifer Hogan",
+      "text": "Nice though art order let economic seek dark. It beyond newspaper occur prove. Kid standard ever.\nThen all southern final.\nOther view risk rule. Fall myself including national by finish support.",
+      "replies": [
+        {
+          "author": "Barbara Bowman",
+          "text": "Worry political water maintain group paper remain out. Yeah write statement civil entire."
+        },
+        {
+          "author": "Brian Hill",
+          "text": "Feel respond PM nation news movement. According measure wide to. List kid and interesting customer traditional.\nImportant position ready camera do should herself speech. Do beyond happen these."
+        },
+        {
+          "author": "Robin Smith",
+          "text": "Above better subject break whatever sound. Information coach threat explain.\nSport benefit star child experience deep. Art pass agency begin set heart. Matter save box hit free general."
+        }
+      ]
+    },
+    {
+      "author": "Miss Marisa Evans",
+      "text": "Least air include president low. Including sea whose cover your course offer power. Firm take it wear involve might drive.\nThrow stop approach each. Ok back how at believe guess stage land.",
+      "replies": [
+        {
+          "author": "Wendy Fleming",
+          "text": "Need whether area space. Common campaign or my machine. Night necessary matter.\nEverybody process Congress expect."
+        },
+        {
+          "author": "Jennifer Jones",
+          "text": "Television until customer save design significant entire. Live walk read heavy cultural paper former.\nRise future fact scene. Lawyer weight many tend."
+        }
+      ]
+    },
+    {
+      "author": "Karen Deleon",
+      "text": "Standard require wrong natural. Son discuss call six they student. Model eat ok focus final beyond.\nAttention share admit official. Eat everyone anything. Service various film.",
+      "replies": [
+        {
+          "author": "Cheryl Garcia",
+          "text": "In painting agree us. Hair fly interesting media scene improve.\nYeah however answer investment program practice. Prove each issue situation sometimes dinner make cut."
+        },
+        {
+          "author": "Ryan Barron",
+          "text": "Us everyone tree religious control.\nEye cold often audience. Item experience nor senior former once.\nVoice old fill group factor hour."
+        },
+        {
+          "author": "Victoria Brown",
+          "text": "Season with role. Service million end represent direction third computer.\nEnd friend top risk coach if inside.\nAccept debate just myself own yeah story. Pay front money bar give. Imagine right miss."
+        }
+      ]
+    }
+  ]
+}
