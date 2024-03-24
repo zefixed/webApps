50552375c626a17440ff3a75490a1391b4a2da7f
Zef-Hub <gerasenkov1@mail.ru>
2024-03-24 13:15:31 +0300

    Mb fix file_search

--- HWs/HW2/file_search.py
+++ HWs/HW2/file_search.py
@@ -1,17 +1,20 @@
 import os
 import sys
 
+
 def file_search(target_file, current_dir=".") -> str:
     for root, dirs, files in os.walk(current_dir):
-        if target_file not in files: continue
-        
+        if target_file not in files:
+            continue
+
         file_path = root + "\\" + target_file
         with open(file_path) as f:
             s = ""
             for i in range(5):
                 s += f.readline()
-            return s
+            return s.strip()
     return f"Файл {target_file} не найден"
+
 
 if __name__ == "__main__":
     current_directory = os.path.dirname(os.path.abspath(__file__))
--- HWs/HW2/plane_angle.py
+++ HWs/HW2/plane_angle.py
@@ -1,36 +1,39 @@
 import math
 import numpy as np
 
+
 class Point:
     def __init__(self, x: int, y: int, z: int):
-        self.x: int = x      
-        self.y: int = y      
-        self.z: int = z      
-        
+        self.x: int = x
+        self.y: int = y
+        self.z: int = z
+
     def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
-    
+
     def _toList(self) -> list[int]:
         return [self.x, self.y, self.z]
-        
+
     def dot(self, a):
-        return np.dot(self._toList(), a._toList()) 
-        
+        return np.dot(self._toList(), a._toList())
+
     def cross(self, a):
-        return np.cross(self._toList(), a._toList())   
-        
+        return np.cross(self._toList(), a._toList())
+
     def absolute(self):
         return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
 
 def plane_angle(a: Point, b: Point, c: Point, d: Point):
     x = Point(*(b - a).cross(c - b))
     y = Point(*(c - b).cross(d - c))
     return f"{math.degrees(math.acos(x.dot(y) / (x.absolute() * y.absolute()))):.2f}"
-    
+
 
 if __name__ == '__main__':
     # print(plane_angle(Point(0, 1, 0), Point(0, 0, 0), Point(1, 0, 0), Point(0, 0, 1)))
-    print(plane_angle(Point(*list(map(int, input().split()))), Point(*list(map(int, input().split()))), Point(*list(map(int, input().split()))), Point(*list(map(int, input().split())))))
+    print(plane_angle(Point(*list(map(int, input().split()))), Point(*list(map(int, input().split()))),
+          Point(*list(map(int, input().split()))), Point(*list(map(int, input().split())))))
